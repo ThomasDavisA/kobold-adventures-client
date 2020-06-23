1a63ebfbe2678766769f4cee1d8e697e5c0a61ab
Thomas Davis <ThomasDavisA@gmail.com>
2020-06-23 10:37:27 -0700

    updated Status Page

--- src/component/StatusBar.js
+++ src/component/StatusBar.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+import KoboldsContext from '../context/KoboldContext';
 
 export default class StatusBar extends Component {
-
+    static contextType = KoboldsContext;
 
     render() {
         return (
@@ -10,11 +11,11 @@ export default class StatusBar extends Component {
                     <canvas className="kobold-portrait"></canvas>
                     <div className="bar-box">
                         <div className="bar-container">
-                            <h3>Life:</h3>
+                            <h3>Life: <span>{this.context.kobold.kobold_health}</span></h3>
                             <div className="bar-background"><div className="life-bar"></div></div>
                         </div>
                         <div className="bar-container">
-                            <h3>Energy:</h3>
+                            <h3>Energy:<span>{this.context.kobold.kobold_energy}</span></h3>
                             <div className="bar-background"><div className="energy-bar"></div></div>
                         </div>
                     </div>
--- src/routes/StatusPage.js
+++ src/routes/StatusPage.js
@@ -1,29 +1,30 @@
 import React, { Component } from 'react';
 import StatusBar from '../component/StatusBar';
+import KoboldsContext from '../context/KoboldContext';
 
 export default class StatusPage extends Component {
-
+    static contextType = KoboldsContext;
 
     render() {
         return (
             <>
                 <h2>Status!</h2>
 
-                <h2>KoboldUser333</h2>
-                <div class="level-box">
+                <h2>{this.context.kobold.kobold_name}</h2>
+                <div className="level-box">
                     <h3>Player Level: 3</h3>
-                    <h3>Kobold Level: 5</h3>
+                    <h3>Kobold Level: {this.context.kobold.kobold_level}</h3>
                 </div>
 
 
                 <div class="stats-box">
                     <h3>Stats</h3>
                     <div class="stats-total">
-                        <h4>Muscle: 8 + 4</h4>
-                        <h4>Fitness: 8 + 3</h4>
-                        <h4>Eloquence: 8 + 5</h4>
-                        <h4>Intellect: 8 + 2</h4>
-                        <h4>Mana: 8 + 5</h4>
+                        <h4>Muscle: {this.context.kobold.kobold_muscle}</h4>
+                        <h4>Fitness: {this.context.kobold.kobold_fitness}</h4>
+                        <h4>Eloquence: {this.context.kobold.kobold_eloquence}</h4>
+                        <h4>Intellect: {this.context.kobold.kobold_intellect}</h4>
+                        <h4>Mana: {this.context.kobold.kobold_mana}</h4>
                     </div>
                 </div>
 
